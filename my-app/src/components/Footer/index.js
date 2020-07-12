097b6414de3104beac171c7874950ee4f940efc5
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-07-11 20:32:34 -0500

    finished components

---
+++ my-app/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">Bianca's Clicky Game &copy; 2020</div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-app/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Clicky Game!</h1>
+      <h2>
+        Click on an image to earn points, but don't click on the same image more
+        than once!
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
